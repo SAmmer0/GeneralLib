cd95f544a9d303568cab24f465e8e27763924e5d
Li Hao <howardlee_h@outlook.com>
2017-03-30 15:12:54 +0800

    添加alpha和beta等其他指标

--- report.py
+++ report.py
@@ -23,6 +23,8 @@ __version__ = 1.1
 修改内容：
     1. 重构了最大回撤函数
     2. 重构了信息比率和夏普比率计算函数
+    3. 添加了计算简单beta和简单alpha的函数
+    4. 添加了DataFrame转化为html表格的类
 """
 __version__ = 1.1
 import pandas as pd
@@ -125,8 +127,8 @@ def info_ratio(retValues, retFreq, benchMark=.0, columnName=None):
         benchMark = pd.Series([datatoolkits.retfreq_trans(benchMark, 1 / retFreq)] * len(retValues),
                               index=retValues.index)
     excessRet = retValues - benchMark
-    annualized_ret = datatoolkits.retfreq_trans(excessRet.mean(), retFreq)
-    annualized_std = datatoolkits.annualize_std(excessRet.std(), excessRet.mean(), retFreq)
+    annualized_ret = datatoolkits.annualize_ret(excessRet, retFreq)
+    annualized_std = datatoolkits.annualize_std(excessRet, retFreq)
     return annualized_ret / annualized_std
 
 
@@ -146,3 +148,164 @@ def sharp_ratio(retValues, retFreq, riskFreeRate=.0, columnName=None):
         sharpRatio 即根据上述公式计算的夏普比率
     '''
     return info_ratio(retValues, retFreq, riskFreeRate, columnName)
+
+
+def cal_rawbeta(rets, benchMark):
+    '''
+    计算收益的简单beta
+    计算方法如下：
+        beta = cov(rets, benchMark)/var(benchMark)
+    @param:
+        rets: 需要评估的收益率序列，要求为pd.Series格式
+        benchMark: 基准收益率序列，要求为pd.Series格式
+    @return:
+        beta值
+    '''
+    return rets.cov(benchMark) / benchMark.var()
+
+
+def cal_rawalpha(rets, benchMark, freq, riskFreeRate=0.):
+    '''
+    计算收益的简单alpha
+    计算方法如下：
+        alpha = annualized_ret - riskFreeRate - beta*(annualized_benchMark - riskFreeRate)
+    @param:
+        rets: 收益率序列，要求为pd.Series格式
+        benchMark: 收益率序列，要求为pd.Series格式
+        freq: 转换频率，例如，日收益率序列对应250，月收益率序列对应12
+        riskFreeRate: 无风险利率，年化
+    @return:
+        alpha值
+    '''
+    annualized_ret = datatoolkits.annualize_ret(rets, freq)
+    annualized_benchMark = datatoolkits.annualize_ret(benchMark, freq)
+    beta = cal_rawbeta(rets, benchMark)
+    return annualized_ret - riskFreeRate - beta * (annualized_benchMark - riskFreeRate)
+
+# 方便写Markdown报告的表格工具
+
+
+class HTMLTable(object):
+
+    '''
+    使用方法：
+        使用该模块所提供的table_convertor对象，调用foramt_df函数，其中函数中的formater参数
+        可以依靠ColFormater类的对象formater来生成
+        例如：
+        test_data = datatoolkits.load_pickle(r"F:\GeneralLib\CONST_DATAS\htmltable.pickle")
+        res = table_convertor.format_df(test_data.reset_index(),
+                                        formater={'nav': formater.get_modformater('pctnp', 3),
+                                                  'CSI700': formater.get_modformater('pctnp', 2)},
+                                        order=['nav', 'index', 'CSI700'])
+    '''
+
+    def __init__(self):
+        self.table = '<table>\n{content}</table>'
+        self.row = '<tr>{content}</tr>'
+        self.col = '<td>{content}</td>'
+        self.row_lists = list()
+
+    def _format_col(self, col_content):
+        return self.col.format(content=col_content)
+
+    def _format_row(self, row_contents):
+        content = ''
+        for c in row_contents:
+            content += self._format_col(c)
+        res = self.row.format(content=content)
+        res += '\n'
+        return res
+
+    def add_header(self, header):
+        '''
+        添加首行标题
+        @param:
+            contents: 需要添加的数据，要求为可迭代形式
+        '''
+        row = self._format_row(header)
+        self.row_lists.insert(0, row)
+
+    def add_row(self, content):
+        '''
+        向行添加内容
+        '''
+        row = self._format_row(content)
+        self.row_lists.append(row)
+
+    def _trans_df(self, df, formater=None, order=None):
+        '''
+        将DataFrame的内容转换为字符串的形式
+        @param:
+            df: 需要转换的DataFrame
+            formater: 默认为None，即对于任何数据，使用str函数转化，可以自行提供，要求为
+                字典形式{col: format_func}，目前只支持根据列自定义转化方式
+            order: 数据列的顺序，默认为None，即使用DataFrame的默认数据列顺序，如果提供，则需要
+                将所有需要生成报表的列都写出
+        @return:
+            转化后的DataFrame，内容全部为字符串形式，index部分会被忽视
+        '''
+        new_df = df.copy()
+        if formater is None:
+            formater = dict()
+        for col in new_df.columns:  # 设置其他没有提供formater的列的格式化的方法
+            formater.setdefault(col, str)
+        if order is not None:   # 改变顺序
+            new_df = new_df.loc[:, order]
+        for col in new_df:
+            new_df.loc[:, col] = new_df[col].apply(formater[col])
+        return new_df
+
+    def format_df(self, df, formater=None, order=None):
+        '''
+        将DataFrame转化为html
+        @param:
+            df: 需要转化的DataFrame
+            formater: 提供的formater方法，默认为None，可以自定义，要求必须为字典形式{col: format_func}，
+                目前只支持根据列自定义转化方式
+            order: 数据列的顺序，默认为None，即使用DataFrame的默认数据列顺序，如果提供，则需要
+                将所有需要生成报表的列都写出
+        '''
+        df_str = self._trans_df(df, formater, order)
+        self.add_header(df_str.columns)
+        for idx in range(len(df_str)):
+            self.add_row(df_str.iloc[idx].tolist())
+        res_str = ''.join(self.row_lists)
+        res_str = self.table.format(content=res_str)
+        return res_str
+
+
+class ColFormater(object):
+
+    def __init__(self):
+        # 目前提供的便捷formater
+        self.formaters = {'date': lambda x: x.strftime('%Y-%m-%d'),
+                          'float2p': lambda x: '%.2f' % x,
+                          'pct2p': lambda x: '%.2f%%' % (100 * x)}
+        # 提供的一些基础formater的原型，可以用于自定义
+        self.prototypes = {'date': lambda y: lambda x: x.strftime(y),
+                           'floatnp': lambda y: lambda x: '%.{}f'.format(y) % x,
+                           'pctnp': lambda y: lambda x: '%.{}f%%'.format(y) % (100 * x)}
+
+    def get_basicformater(self, formater_type):
+        assert formater_type in self.formaters, 'Error, valid formaters are {}'.\
+            format(list(self.formaters.keys()))
+        return self.formaters[formater_type]
+
+    def get_modformater(self, formater_type, param):
+        '''
+        用于使用基础formater原型生成自定义的formater
+        '''
+        assert formater_type in self.prototypes, 'Error, valid formaters are {}'.\
+            format(list(self.formaters.keys()))
+        return self.prototypes[formater_type](param)
+
+
+formater = ColFormater()
+table_convertor = HTMLTable()
+
+if __name__ == '__main__':
+    test_data = datatoolkits.load_pickle(r"F:\GeneralLib\CONST_DATAS\htmltable.pickle")
+    res = table_convertor.format_df(test_data.reset_index(),
+                                    formater={'nav': formater.get_modformater('pctnp', 3),
+                                              'CSI700': formater.get_modformater('pctnp', 2)},
+                                    order=['nav', 'index', 'CSI700'])
