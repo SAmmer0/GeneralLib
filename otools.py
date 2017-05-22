7dd8249eac5f369f1268f512f3f80a07fa338213
Li Hao <howardlee_h@outlook.com>
2017-05-22 15:13:53 +0800

    初始化

---
+++ otools.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-05-22 15:09:24
+# @Author  : Li Hao (howardlee_h@outlook.com)
+# @Link    : https://github.com/SAmmer0
+# @Version : $Id$
+'''
+__version__ = 1.0.0
+修改日期：2017-05-22
+修改内容：
+    初始化，添加name_wrapper
+'''
+
+
+def name_wrapper(func):
+    '''
+    装饰器，用于打印当前正在执行的函数名称
+    Parameter
+    ---------
+    func: function
+        需要被打印的函数
+
+    Return
+    ------
+    out: function
+        修改后的函数
+    '''
+    def inner(*args, **kwargs):
+        print('Procedure {name}'.format(name=func.__name__))
+        res = func(*args, **kwargs)
+        return res
+    return inner
