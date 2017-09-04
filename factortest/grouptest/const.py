7936e8ebb62f067616dd29fdfd662fb03c7817ec
Hao Li <howardleeh@gmail.com>
2017-09-04 18:05:02 +0800

    添加一些常量

---
+++ factortest/grouptest/const.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-08-26 17:20:43
+# @Author  : Li Hao (howardlee_h@outlook.com)
+# @Link    : https://github.com/SAmmer0
+# @Version : $Id$
+
+'''
+一些常量定义
+'''
+
+# 回测的组合权重计算方式
+EQUAL_WEIGHTED = 'equal-weighted'
+TOTALMKV_WEIGHTED = 'totalmkv-weighted'
+FLOATMKV_WEIGHTED = 'floatmkv-weighted'
+
+# 回测的换仓频率
+MONTHLY = 'monthly'
+WEEKLY = 'weekly'
