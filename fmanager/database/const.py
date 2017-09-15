c8a5bf27c63576f83d4567e4eaff58902b0f0833
Hao Li <howardleeh@gmail.com>
2017-09-15 13:27:58 +0800

    将常量文件独立出来

---
+++ fmanager/database/const.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-09-15 10:57:39
+# @Author  : Li Hao (howardlee_h@outlook.com)
+# @Link    : https://github.com/SAmmer0
+# @Version : $Id$
+from datetime import datetime
+
+MAX_COL_SIZE = 4000
+NaS = 'NaS'
+FIRST_TRADING_DAY = datetime(1990, 1, 1)
